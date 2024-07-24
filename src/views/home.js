88646169c60cd9ad8037bb26766f82eb9c7df7b0
LongAthelstan <longaluminium10@gmail.com>
2024-07-24 17:23:47 +0700

    update release

--- src/views/home.js
+++ src/views/home.js
@@ -16,15 +16,21 @@ const Home = (props) => {
         <img alt="image" src="/alt-exam-300h.png" className="home-image1" />
         <span className="home-text">Alt-Exam</span>
         <div className="home-container2">
+        <a href="https://github.com/longathelstan/Alt-Exam-Web/releases/download/Release/Alt-Exam-TP_1.0.0_x64_en-US.msi" target="_blank">
           <button type="button" className="home-button button">
             Alt-Exam-TP
           </button>
+        </a>
+        <a href='https://github.com/longathelstan/Alt-Exam-Web/releases/download/Release/Alt-Exam-SP_1.0.0_x64_en-US.msi' target='_blank'>
           <button type="button" className="home-button1 button">
             Alt-Exam-SP
           </button>
+        </a>
+        <a href='https://github.com/longathelstan/Alt-Exam-Web/releases/download/Release/Authentication.exe'>
           <button type="button" className="home-button2 button">
             Authentication
           </button>
+        </a>
         </div>
       </div>
     </div>
